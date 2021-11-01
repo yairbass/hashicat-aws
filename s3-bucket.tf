6cd240cee9cdf975784f97f83cd1a79bbe94521c
yairbass <yairbass@gmail.com>
2021-11-01 14:07:58 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -4,9 +4,16 @@ module "s3_bucket" {
   source  = "app.terraform.io/YairBass-Chip/s3-bucket/aws"
   version = "2.2.0"
 
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
   acceleration_status = "false"
-  bucket = "${var.s3_bucket_bucket}"
+  bucket = " ${bucket-prefix}-bucket"
   bucket_prefix = "yair-bass"
-  policy = "${var.s3_bucket_policy}"
-  request_payer = "${var.s3_bucket_request_payer}"
+ 
+  versioning = {
+    enabled = true
+  }
+ 
+ 
 }
